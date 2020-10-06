3f9e9396e7427793e19b564e5afc5de04870df62
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-10-06 15:59:07 +0300

    commit

--- validation/loginSchema.js
+++ validation/loginSchema.js
@@ -3,7 +3,7 @@ const Joi = require("@hapi/joi");
 const schema = Joi.object({
   email: Joi.string().required().email().max(255),
   password: Joi.string().required().max(1024),
-  rememberMe: Joi.required(),
+  rememberMe: Joi.bool().required(),
 });
 
 const validateInputAsync = (data) => {
