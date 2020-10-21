fe2a9e66c6f73d7d210cf01ccd011ba116cf309d
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-10-21 15:50:37 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,16 +17,14 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: false,
 })
-.catch((err)=>console.log('error initiat mongo', err))
-
 mongoose.connection.on("connected", () => {
   console.log("Connected to MONGO");
-});
+})
 mongoose.connection.on("error", (err) => {
   console.log("error connecting to MONGO", err);
-});
+})
 
 app.use(logger("dev"));
 app.use(cors());
