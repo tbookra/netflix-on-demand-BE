f824aaab39c0680e4485db7f4df023e40d1f6334
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-10-22 17:31:02 +0300

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const app = express();
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useCreateIndex: true,
-  useUnifiedTopology: false,
+  useUnifiedTopology: true,
 })
 mongoose.connection.on("connected", () => {
   console.log("Connected to MONGO");
