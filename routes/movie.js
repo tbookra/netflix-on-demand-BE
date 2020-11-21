bdc1efa25c1ecbe45530f36eed32c78642ecdece
aviram5 <60235058+aviram5@users.noreply.github.com>
2020-11-21 15:09:23 +0200

    Update movie.js

--- routes/movie.js
+++ routes/movie.js
@@ -1,14 +1,28 @@
 const router = require("express").Router();
-const movieController = require('../controllers/movieController');
-const passwordToModify = require('../middleware/passwordToModify');
+const movieController = require("../controllers/movieController");
+const passwordToModify = require("../middleware/passwordToModify");
+const Users = require("../models/mongoDB/User");
 
-router.get('/checkIfMovieAccessible/:movieId',passwordToModify, movieController.getMovie)
+router.get(
+  "/checkIfMovieAccessible/:movieId",
+  passwordToModify,
+  movieController.getMovie
+);
 
-router.post('/addMovie', movieController.addMovie)
+router.post("/addMovie", movieController.addMovie);
 
-router.post('/buyMembership', async(req,res)=>{
-    //change user membership and validate
-})
-
+router.post("/buyMembership", async (req, res) => {
+  const { email } = req.body;
+  try {
+    const user = await Users.findOne({ email });
+    const isMember = user.get("isMember");
+    if (isMember) return res.json({ membership: "you already a member" });
+    user.isMember = true;
+    user.save();
+    res.json({ membership: "successfully member" });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
